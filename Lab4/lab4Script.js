04cd80043263d9782ea125533d00512dd6f9966e
Ethan Luna <ethanlunacado@gmail.com>
2024-02-15 10:16:24 -0600

    agregamos toda la parte de JS

---
+++ Lab4/lab4Script.js
@@ -0,0 +1,14 @@
+console.log("Hola Mundo");
+console.info("Esto es info!")
+console.warn("Esto es un CUIDADO");
+console.error("Esto es un ERROR");
+
+//variables, constantes
+//Forma antigua de declarar varibles
+var nombre = "Ethan";
+
+//Forma moderna y mas segura
+let precio = 12.54;
+
+//Constantes
+const stock = 34;

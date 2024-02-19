70e4816d075aecac837583a40ea84d1b7a19ebbb
Ethan Luna <ethanlunacado@gmail.com>
2024-02-19 12:12:59 -0600

    resolvi el Lab6

--- Lab6/app.js
+++ Lab6/app.js
@@ -28,12 +28,22 @@ function verify() {
     
     const symbols = /[!@#$%&]/g.test(p);
     const numbers = /[0-9]/g.test(p);
+    const capital = /[A-Z]/g.test(p);
 
     const warnSymbol = document.getElementById("symbolWarning");
     const warnNum = document.getElementById("numberWarning");
+    const warnCapital = document.getElementById("capitalWarning");
+    const successMessage = document.getElementById("successMessage");
 
     warnSymbol.textContent = symbols ? "" : "Asegúrate de que la contraseña incluya símbolos";
     warnNum.textContent = numbers ? "" : "Asegúrate de que la contraseña incluya números";
+    warnCapital.textContent = capital ? "" : "Asegúrate de que la contraseña incluya mayúsculas";
+    
+    if (warnSymbol.textContent === "" && warnNum.textContent === "" && warnCapital.textContent === "") {
+        successMessage.textContent = "La contraseña ha sido validada";
+    } else {
+        successMessage.textContent = "";
+    }
   }
 
 document.getElementById("eye").addEventListener("click", decideToShow, false); // Removed parentheses after function name

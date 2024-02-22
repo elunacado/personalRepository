44443b020d0236605764a094f043d91a19c54e96
Ethan Luna <ethanlunacado@gmail.com>
2024-02-22 10:48:42 -0600

    Añadimos el server

--- Lab8/app.js
+++ Lab8/app.js
@@ -2,6 +2,17 @@ console.log("Hola Ethan desde Node");
 
 //Nos permite acceder y manipular los files de Node
 const fileSystem =  require('fs');
+//crea un server pequeño
+const http = require('http');
+const { loadavg } = require('os');
 
 //Escribe en el archivo indicado el segundo parametro
-fileSystem('hola.txt', 'hola de nuevo archivo!')
+//fileSystem('hola.txt', 'hola de nuevo archivo!')
+
+const server = http.createServer((req,resp) => {
+    resp.setHeader('Content-Type', 'text/html');
+    resp.write("Hola Ethan desde un html de Node")
+    resp.end();
+})
+
+server.listen(3000);

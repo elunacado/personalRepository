b3f234011161fdf604d69a9f3c60ca77eff5eae0
Ethan Luna <ethanlunacado@gmail.com>
2024-02-26 10:02:54 -0600

    agregue contenido a las branches

--- Lab8/app.js
+++ Lab8/app.js
@@ -1,23 +1,55 @@
-console.log("Hola Ethan desde Node");
-
-//Nos permite acceder y manipular los files de Node
-const fileSystem =  require('fs');
-//crea un server pequeño
+const fileSystem = require('fs');
 const http = require('http');
-const { loadavg } = require('os');
 
-//Escribe en el archivo indicado el segundo parametro
-//fileSystem('hola.txt', 'hola de nuevo archivo!')
-
-const server = http.createServer((req,resp) => {
-    if(req.url=="/"){
-        resp
-    } else if (req.url=="/construir") {
-        
+const server = http.createServer((req, resp) => {
+    if (req.url === "/") {
+        resp.write(
+            `<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+            </head>
+            <body>
+                <h1>Bienvenido a la taqueria ¿De que quiere?</h1>
+            </body>
+            </html>`
+        );
+        resp.end();
+    } else if (req.url === "/pastor") {
+        resp.write(
+        `<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+            </head>
+            <body>
+                <h1>Salen dos de pastor con piña</h1>
+            </body>
+            </html>`);
+        resp.end();
     } else {
         resp.statusCode = 404;
+        resp.write(
+            `<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+            </head>
+            <body>
+                <h1>404: No hay tortillas joven</h1>
+            </body>
+            </html>`
+        );
         resp.end();
     }
-})
+});
 
-server.listen(3000);
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});

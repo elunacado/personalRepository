36d6caf5a46577059ea4370ed1aad93cfae3c1e4
Ethan Luna <ethanlunacado@gmail.com>
2024-02-29 10:46:14 -0600

    this is Lab12

--- Lab11/app.js
+++ Lab11/app.js
@@ -4,12 +4,16 @@ const bodyParser = require('body-parser');
 const rutas = require('./routes/main.routes');
 const path = require('path');
 
+app.set('view-engine','ejs');
+app.set('views', 'views')
+
 app.use('/',rutas);
+app.use(express.static(path.join(__dirname, 'views','404.html')))
+
+
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(       
-         path.join(__dirname, 'views', '404.html')
-    );
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
   
 
--- Lab11/routes/main.routes.js
+++ Lab11/routes/main.routes.js
@@ -18,18 +18,7 @@ router.get('/',(req, res, next) =>{
 })
 
 router.get('/trayectoria-educativa',(req, res, next) => {
-    res.send(`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Saying Hi</title>
-    </head>
-    <body>
-        <h1>Hi EDUCATION</h1>
-    </body>
-    </html>
-    `)
+    res.render('escolaridad.ejs')
     next();
     
 });
